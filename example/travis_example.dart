a55ba2a79fb301e3c8d871543a04b75545ba8ed3
Kevin Moore <kevmoo@google.com>
2015-07-21 10:33:53 -0700

    use StackTrace in example to better diagnose errors

--- example/travis_example.dart
+++ example/travis_example.dart
@@ -1,27 +1,35 @@
 library travis.example;
 
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:stack_trace/stack_trace.dart';
 import 'package:travis/travis.dart';
 
 main(List<String> args) async {
-  var client = new Travis(args.single);
+  await Chain.capture(() async {
+    var client = new Travis(args.single);
 
-  var response = await client.repos(member: 'kevmoo');
+    var response = await client.repos(member: 'kevmoo');
 
-  for (Repo r in response) {
-    print(r.slug);
+    for (Repo r in response) {
+      print(r.slug);
 
-    List<Build> builds = await client.builds(ids: [r.lastBuildId]);
+      List<Build> builds = await client.builds(ids: [r.lastBuildId]);
 
-    var build = builds.first;
+      var build = builds.first;
 
-    print([build.id, build.jobIds]);
+      print([build.id, build.jobIds]);
 
-    for (int jobId in build.jobIds) {
-      var job = await client.job(jobId);
+      for (int jobId in build.jobIds) {
+        var job = await client.job(jobId);
+      }
     }
-  }
+  }, onError: (error, chain) {
+    print(error);
+    print(chain.terse);
+    exit(1);
+  });
 }
 
 const _encoder = const JsonEncoder.withIndent('  ');
